11913b8489149f0913c2f4bc763e4c5b0985d94f
moondev03 <moondev03@gmail.com>
2024-05-23 08:54:53 +0900

    feat | BaseFragment 작성

--- DeliveryService/app/src/main/java/com/mju/deliveryservice/presentation/view/MainActivity.kt
+++ DeliveryService/app/src/main/java/com/mju/deliveryservice/presentation/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.mju.deliveryservice
+package com.mju.deliveryservice.presentation.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mju.deliveryservice.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
