499d7799d0a5966a7a23d34ca690b07c01efbf96
박성훈 <99787274+houony@users.noreply.github.com>
2024-06-06 21:13:31 +0900

    Create OrderFragment.kt

---
+++ DeliveryService/app/src/main/java/com/mju/deliveryservice/presentation/view/order/OrderFragment.kt
@@ -0,0 +1,46 @@
+package com.mju.deliveryservice.presentation.view.order
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.mju.deliveryservice.R
+import com.mju.deliveryservice.databinding.FragmentOrderBinding
+
+class OrderFragment : Fragment() {
+
+    private var _binding: FragmentOrderBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: OrderViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOrderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupListeners()
+
+    }
+
+    private fun setupListeners() {
+        binding.btnPurchase.setOnClickListener {
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fl_home, OrderCompleteFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
