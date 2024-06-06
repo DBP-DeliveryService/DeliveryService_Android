14286cada2fc34399e1ad8d674aa4a83ec4f9b4b
박성훈 <99787274+houony@users.noreply.github.com>
2024-06-06 21:20:46 +0900

    Update CartFragment.kt

--- DeliveryService/app/src/main/java/com/mju/deliveryservice/presentation/view/cart/CartFragment.kt
+++ DeliveryService/app/src/main/java/com/mju/deliveryservice/presentation/view/cart/CartFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+//import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import com.mju.deliveryservice.R
